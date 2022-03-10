3e114e967caa745c1ca4ffddeda2904106068849
Jamie MacLeod <jamie@primerapi.com>
2022-03-10 18:09:40 +0000

    Cleanish code

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-import discord
-import json
-from discord.ext import commands
-import datetime
-import time
-import random
 import asyncio
+import json
+import random
+import time
+from collections import namedtuple
+from functools import partial
+from typing import Optional
 
-# These are annoying but fix things
-time.sleep(0.5)
+import discord
+from discord import ActivityType
+from discord.ext import commands
+
+Giveaway = namedtuple("Giveaway", ["channel", "duration", "count", "item_name"])
 
 with open("config.json") as file:
     info = json.load(file)
@@ -15,98 +18,100 @@ with open("config.json") as file:
     prefix = info["prefix"]
     status = info["status"]
     icon_url = info["icon_url"]
+    emoji = info["emoji"]
 
-time.sleep(0.5)
-
-client = commands.Bot(command_prefix=prefix, intents=discord.Intents.all())
-# We don't want the help command
-client.remove_command("help")
-
-time.sleep(0.5)
+client = commands.Bot(
+    command_prefix=prefix, help_command=None, intents=discord.Intents.all()
+)
 
 
-@client.event
-async def on_ready():
-    print("[Connected]")
-    activity = discord.Game(name=status, type=3)
-    await client.change_presence(status=discord.Status.online, activity=activity)
+def _check_context_matches(ctx_a, ctx_b):
+    return ctx_a.author == ctx_b.author and ctx_a.channel == ctx_b.channel
 
 
-@client.command()
-@commands.has_permissions(manage_channels=True)
-async def giveaway(ctx):
-    giveaway_questions = [
-        "Which channel will I host the giveaway in?",
-        "How long should the giveaway run for (in seconds)?",
-        "How many things should I giveaway?",
-        "What things should I giveaway?",
-    ]
-    giveaway_answers = []
-
-    def check(m):
-        return m.author == ctx.author and m.channel == ctx.channel
-
-    for question in giveaway_questions:
-        await ctx.send(question)
-        try:
-            message = await client.wait_for("message", timeout=30.0, check=check)
-        except asyncio.TimeoutError:
-            await ctx.send(
-                "You didn't answer in time.  Please try again and be sure to send your answer within 30 seconds of the question."
-            )
-            return
-        else:
-            giveaway_answers.append(message.content)
+async def ask_question(ctx, question: str) -> Optional[str]:
+    await ctx.send(question)
 
     try:
-        c_id = int(giveaway_answers[0][2:-1])
+        message = await client.wait_for(
+            "message", timeout=30.0, check=partial(_check_context_matches, ctx)
+        )
+    except asyncio.TimeoutError:
+        await ctx.send(
+            "You didn't answer in time.  Please try again and be sure to send your answer within 30 seconds of the question."
+        )
+        return
+    return message.content
+
+
+async def gather_arguments(ctx) -> Optional[Giveaway]:
+    giveaway_answers = [
+        await ask_question(ctx, question)
+        for question in [
+            "Which channel will I host the giveaway in?",
+            "How long should the giveaway run for (in seconds)?",
+            "How many things should I giveaway?",
+            "What things should I giveaway?",
+        ]
+    ]
+    channel_id, duration, giveaway_count, giveaway_item = giveaway_answers
+
+    try:
+        channel_id = int(channel_id[2:-1])
     except:
         await ctx.send(
             f"You failed to mention the channel correctly.  Please do it like this: {ctx.channel.mention}"
         )
         return
 
-    channel = client.get_channel(c_id)
-    time = int(giveaway_answers[1])
-
-    await ctx.send(
-        f"The {giveaway_answers[3]} giveaway will begin shortly.\nPlease direct your attention to {channel.mention}, this giveaway will end in {time} seconds."
+    return Giveaway(
+        duration=int(duration),
+        channel=client.get_channel(channel_id),
+        count=giveaway_count,
+        item_name=giveaway_item,
     )
 
+
+async def start_giveaway(ctx, args: Giveaway) -> int:
     give = discord.Embed(color=0x2ECC71)
     give.set_author(
-        name=f"{giveaway_answers[2]} {giveaway_answers[3].upper()} GIVEAWAY TIME!",
+        name=f"{args.count} {args.item_name.upper()} GIVEAWAY TIME!",
         icon_url=icon_url,
     )
     give.add_field(
-        name=f"{giveaway_answers[2]} {giveaway_answers[3]} Giveaway!",
-        value=f"React with 🐒 to enter!\n Ends in {round(time/60, 2)} minutes!",
+        name=f"{args.count} {args.item_name} Giveaway!",
+        value=f"React with {emoji} to enter!\n Ends in {round(args.duration / 60, 2)} minutes!",
         inline=False,
     )
     give.set_footer(text=f"Hosted By {ctx.author.name}")
-    my_message = await channel.send("@everyone", embed=give)
+    my_message = await args.channel.send("@everyone", embed=give)
 
-    await my_message.add_reaction("🐒")
-    await asyncio.sleep(time)
+    await my_message.add_reaction(emoji)
 
-    new_message = await channel.fetch_message(my_message.id)
+    return my_message.id
 
-    users = await new_message.reactions[0].users().flatten()
-    _num_winners = int(giveaway_answers[2])
-    if len(users) < _num_winners:
-        _num_winners = len(users)
-    if _num_winners == 0:
+
+async def end_giveaway(message_id: int, args: Giveaway) -> None:
+    giveaway_message = await args.channel.fetch_message(message_id)
+
+    users = await giveaway_message.reactions[0].users().flatten()
+    users = list(filter(lambda x: not x.bot, users))
+
+    num_winners = min(int(args.count), len(users))
+
+    if not num_winners:
         return
-    winners = random.sample(users, _num_winners)
+
+    winners = random.sample(users, num_winners)
     winners = list(dict.fromkeys(winners))
 
     winning_announcement = discord.Embed(color=0xFF2424)
     winning_announcement.set_author(
-        name=f"{giveaway_answers[3].upper()} GIVEAWAY HAS ENDED!", icon_url=icon_url
+        name=f"{args.item_name.upper()} GIVEAWAY HAS ENDED!", icon_url=icon_url
     )
     winner_names = ",".join([winner.mention for winner in winners])
     winning_announcement.add_field(
-        name=f"{giveaway_answers[3]} giveaway",
+        name=f"{args.item_name} giveaway",
         value=f"🥳 **Winner**: {winner_names}\n 🎫 **Number of Entrants**: {len(users)}",
         inline=False,
     )
@@ -114,15 +119,37 @@ async def giveaway(ctx):
     winning_announcement.set_footer(
         text="Thanks for entering! We will bring the winner into a private channel! WE WILL NOT DM YOU."
     )
-    await channel.send(embed=winning_announcement)
+    await args.channel.send(embed=winning_announcement)
+
+
+@client.event
+async def on_ready():
+    print("[Connected]")
+    activity = discord.Game(name=status, type=ActivityType.watching)
+    await client.change_presence(status=discord.Status.online, activity=activity)
+
+
+@client.command()
+@commands.has_permissions(manage_channels=True)
+async def giveaway(ctx):
+
+    args = await gather_arguments(ctx)
+    if not args:
+        return
+
+    await ctx.send(
+        f"The {args.item_name} giveaway will begin shortly.\nPlease direct your attention to {args.channel.mention}, this giveaway will end in {args.duration} seconds."
+    )
+
+    message_id = await start_giveaway(ctx, args)
+    await asyncio.sleep(args.duration)
+    await end_giveaway(message_id, args)
 
 
 @client.event
 async def on_command_error(ctx, error):
-    await ctx.send(f"{ctx} {error}")
+    return
 
 
-time.sleep(0.5)
-
-
-client.run(token)
+if __name__ == "__main__":
+    client.run(token)
